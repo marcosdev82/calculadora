ef78ccda848855093f93d44fdd0b2c847d742957
mikek182 <marcosvinicius@geekwork.com.br>
2024-08-15 20:49:07 -0300

    Implementação das operação na função calcular com o teste tdd

---
+++ src/calculadora.service.js
@@ -0,0 +1,43 @@
+function CalculadoraService() {
+
+    const SOMA = '+';
+    const SUBTRACAO = '-';
+    const DIVISAO = '/';
+    const MULTIPLICACAO = '*';
+
+    function calcular(numero1, numero2, operacao) {
+
+        let resultado;
+
+        switch(operacao) {
+            case SOMA:
+                resultado = numero1 + numero2;
+                break;
+            case SUBTRACAO:
+                resultado = numero1 - numero2;
+                break;
+            case DIVISAO:
+                resultado = numero1 / numero2;
+                break;
+            case MULTIPLICACAO:
+                resultado = numero1 * numero2;
+                break;
+            default:
+                resultado = 0;
+        }
+
+        return resultado;
+    }
+
+    // retorna uma referência do calcular
+    return [
+        calcular,
+        SOMA,
+        SUBTRACAO,
+        DIVISAO,
+        MULTIPLICACAO
+    ];
+
+}
+
+export default CalculadoraService;
--- src/calculadora.service.test.js
+++ src/calculadora.service.test.js
@@ -1,15 +1,35 @@
 import React from "react";
 import ReactDOM from 'react-dom';
-import CalculadoraService from "calculadora.service";
+import CalculadoraService from "./calculadora.service";
 
 
 describe('Teste do CalculadoraService', () => {
-
-    const [calcular] = CalculadoraService();
+    // recebe uma referência do calcular
+    const [calcular, SOMA, SUBTRACAO, DIVISAO, MULTIPLICACAO] = CalculadoraService();
 
     it('deve garantir 1 + 4 = 5.', () => {
-        let soma = calcular(1, 4, '+');
+        let soma = calcular(1, 4, SOMA);
         expect(soma).toEqual(5);
-    })
+    });
+
+    it('deve garantir 1 - 4 = -3', () => {
+        let subtracao  = calcular(1, 4, SUBTRACAO)
+        expect(subtracao).toEqual(-3)
+    });
+
+    it('deve garantir 1 / 4 = 0.25', () => {
+        let divisao  = calcular(1, 4, DIVISAO)
+        expect(divisao).toEqual(0.25)
+    });
+
+    it('deve garantir 1 * 4 = 0.25', () => {
+        let multiplicacao  = calcular(1, 4, MULTIPLICACAO)
+        expect(multiplicacao).toEqual(4)
+    });
+
+    it('deve retornar o 0 para operação inválida', () => {
+        let operacaoInvarilida  = calcular(1, 4, '%')
+        expect(operacaoInvarilida).toEqual(0)
+    });
 
 })
