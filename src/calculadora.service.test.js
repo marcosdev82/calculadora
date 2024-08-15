9567ea2b5d0ad27a1f73251972c70a850ab2f5b6
mikek182 <marcosvinicius@geekwork.com.br>
2024-08-15 20:13:57 -0300

    Teste do CalcualService

---
+++ src/calculadora.service.test.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+import CalculadoraService from "calculadora.service";
+
+
+describe('Teste do CalculadoraService', () => {
+
+    const [calcular] = CalculadoraService();
+
+    it('deve garantir 1 + 4 = 5.', () => {
+        let soma = calcular(1, 4, '+');
+        expect(soma).toEqual(5);
+    })
+
+})
