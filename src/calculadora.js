9f55d12b174bdec4fe6e169a1a380362a23dd9c8
mikek182 <marcosvinicius@geekwork.com.br>
2024-08-15 19:22:47 -0300

    Criação do container

--- src/calculadora.js
+++ src/calculadora.js
@@ -1,9 +1,15 @@
 import './calculadora.css';
-import { Jumbotron } from 'react-bootstrap';
+import { Container  } from 'react-bootstrap'; 
 
 function Calculadora() {
   return (
-    <Jumbotron></Jumbotron>
+    <Container style={{
+      background: 'transparent!important',
+      backgroundColor: '#007bff',
+      padding: '5px',
+      margin: '5px',
+      width: '400px',
+    }}></Container >
   );
 }
 
