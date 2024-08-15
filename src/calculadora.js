7bf7d3878ea234de65d034e407c62bf391f1c769
mikek182 <marcosvinicius@geekwork.com.br>
2024-08-15 19:03:36 -0300

    Renomeando arquivo principais

---
+++ src/calculadora.js
@@ -0,0 +1,10 @@
+import './calculadora.css';
+import { Jumbotron } from 'react-bootstrap';
+
+function Calculadora() {
+  return (
+    <Jumbotron></Jumbotron>
+  );
+}
+
+export default Calculadora;
