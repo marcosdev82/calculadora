3c80e640f1cff93892d6a9524d6a716f60d9ac71
mikek182 <marcosvinicius@geekwork.com.br>
2024-08-20 12:48:21 -0300

    Adicionado it no describe

--- src/calculadora.test.js
+++ src/calculadora.test.js
@@ -2,8 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Calculadora from "./calculadora";
 
-it('Deve renderizar o componente sem erros', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<Calculadora />, div);
-    ReactDOM.unmountComponentAtNode(div);
+describe('Calculadora', () => {
+
+    it('Deve renderizar o componente sem erros', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<Calculadora />, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
+
 });
